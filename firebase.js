ba6ed83c9efcd3467e698b1e40a5aab6b6904cd1
janasabhaforum-hu <janasabhaforum@gmail.com>
2026-01-05 19:41:28 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// firebase.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCqxQKxSaNORdePg8xP6-ePmMr40DisFW0",
+  authDomain: "janasabha-app.firebaseapp.com",
+  projectId: "janasabha-app",
+  storageBucket: "janasabha-app.firebasestorage.app",
+  messagingSenderId: "596563440786",
+  appId: "1:596563440786:web:4b8264e45afecc411aa24b"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const auth = firebase.auth();
+const db = firebase.firestore();
